935a0bf623e838b1d4e8833a68ee15dfbe35f98b
restingdemon <akshay.garg130803@gmail.com>
2023-12-23 01:47:55 +0530

    create/update/get/delete apis

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/restingdemon/go-mysql-mux/pkg/routes"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r))
+
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/restingdemon/go-mysql-mux
+
+go 1.21.5
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
